c5aca9b7ab5369b35502605604d3d54cdfe4cf93
yes-im-harsh <harshchauhan.cse279@gmail.com>
2023-01-07 13:56:30 +0530

    SignIn Page done

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
+import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 
 const SignIn = () => {
-  return (
-    <div>SignIn</div>
-  )
-}
+  const [fromData, setFromData] = useState({
+    email: "",
+    password: "",
+  });
 
-export default SignIn
+  const [showPassword, setShowPassword] = useState(false);
+
+  const { email, password } = fromData;
+
+  const handleChange = (e) => {
+    setFromData({ ...FormData, [e.target.id]: e.target.value });
+  };
+
+  return (
+    <div className="pageContainer">
+      <header>
+        <p className="pageHeader"> Welcome Back!</p>
+      </header>
+
+      <form>
+        <input
+          type="email"
+          className="emailInput"
+          placeholder="Email"
+          id="email"
+          value={email}
+          onChange={handleChange}
+        />
+        <div className="passwordInputDiv">
+          <input
+            className="passwordInput"
+            placeholder="Password"
+            type={showPassword ? "text" : "password"}
+            value={password}
+            onChange={handleChange}
+          />
+          <img
+            src={visibilityIcon}
+            alt="eye"
+            className="showPassword"
+            onClick={() => setShowPassword((prevState) => !prevState)}
+          />
+        </div>
+
+        <Link className="forgotPasswordLink" to="/forgot-password">
+          Forgot Password
+        </Link>
+
+        <div className="signInBar">
+          <p className="signInText">Sign In</p>
+          <button className="signInButton">
+            <ArrowRightIcon width="32px" height="32px" fill="#ffffff" />
+          </button>
+        </div>
+      </form>
+
+      {/* Todo: GoogleOAuth */}
+      <Link to="/sign-up" className="registerLink">
+        Sign Up Instead
+      </Link>
+    </div>
+  );
+};
+
+export default SignIn;
